17297337f916cde14c4bf14a08f559629cb95448
Axel Medina <axm27000@gmail.com>
2019-03-17 01:26:51 -0600

    agrega func header

--- ProyFinal/ProyFinal/Source.cpp
+++ ProyFinal/ProyFinal/Source.cpp
@@ -7,16 +7,19 @@
 #include <string.h>
 using namespace std;
 
-
+void header();
 
 int main() {
 
 	setlocale(LC_ALL, "spanish"); //Permite usar acentos del español
-
 	system("cls");
-
-	//intro txt
-	cout << "\t\t\tCONSORCIO FERRETERO MEDINA S.A. de C.V.";
-	system("pause");
+	header();
+	system("pause>nul");
 	return 0;
+}
+
+void header() {
+	/*Esta func imprime texto en la parte superior de la pantalla
+	La uso para simular la permanencia de un elemento de cabecera en la interfaz de usuario cada vez que se limpie la consola*/
+	cout << "\t\t\tCONSORCIO FERRETERO MEDINA S.A. de C.V.";
 }
