5d6efc6faefddc41d2790544c95ba2d1c1cb08a1
Axel Medina <axm27000@gmail.com>
2019-03-17 01:20:34 -0600

    iniciar source y arch de proy

---
+++ ProyFinal/ProyFinal/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+#include <conio.h>
+#include <ctime>
+#include <string.h>
+using namespace std;
+
+
+
+int main() {
+
+	setlocale(LC_ALL, "spanish"); //Permite usar acentos del español
+
+	system("cls");
+
+	//intro txt
+	cout << "\t\t\tCONSORCIO FERRETERO MEDINA S.A. de C.V.";
+	system("pause");
+	return 0;
+}
