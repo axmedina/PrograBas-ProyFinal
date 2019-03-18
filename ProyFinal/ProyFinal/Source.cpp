6dda86ec48151adf33e7745fc85ca7977009bba1
axmedina <axm27000@gmail.com>
2019-03-18 02:06:14 -0600

    actu source menu p

--- ProyFinal/ProyFinal/Source.cpp
+++ ProyFinal/ProyFinal/Source.cpp
@@ -8,20 +8,23 @@
 using namespace std;
 
 void header();
-int opc_menup;
+int opc_menu_p;
 
 int main() {
 
-	setlocale(LC_ALL, "spanish"); //Permite usar acentos del español
-	system("cls");
-	header();
-	cout << "\n\n\tMENU PRINCIPAL";
-	system("pause>nul");
-	return 0;
+	do{
+		setlocale(LC_ALL, "spanish"); //Permite usar acentos del español
+		system("cls");
+		header();
+		cout << "\n\n\tMENU PRINCIPAL";
+		cout << "\n\n\t1 - VENTA de productos\n\t2 - EDITAR productos\n\t3 - MOSTRAR productos\n\t4 - SALIR del sistema";
+		cin >> opc_menu_p
+		system("pause>nul");
+	}while(volver_menu_p==true);
 }
 
 void header() {
 	/*Esta func imprime texto en la parte superior de la pantalla
 	La uso para simular la permanencia de un elemento de cabecera en la interfaz de usuario cada vez que se limpie la consola*/
-	cout << "\tCONSORCIO FERRETERO MEDINA S.A. de C.V.\n\tSucursal López Mateos - Tel: (828) 284 64 24\n\twww.grupomedina.mx\n";
+	cout << "\t\t\tCONSORCIO FERRETERO MEDINA S.A. de C.V.\n\t\t\tSucursal López Mateos\n\t\t\tTel: (828) 284 64 24\n\t\t\t\twww.grupomedina.mx\n";
 }
